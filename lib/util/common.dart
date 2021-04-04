608a5bc0d2d13a24f688688d5085aac790876b14
Rahal-Kanishka <rahalncm@gmail.com>
2021-04-05 02:55:07 +0530

    added common util class

---
+++ lib/util/common.dart
@@ -0,0 +1,17 @@
+
+
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/services.dart';
+
+/// Class to provide common Util functionalities
+class Common {
+
+  static Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    Codec codec = await instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ImageByteFormat.png)).buffer.asUint8List();
+  }
+}
