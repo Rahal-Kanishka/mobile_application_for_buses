93c753881566d1b811d194f2ab22ce48c9ac1e05
Rahal-Kanishka <rahalncm@gmail.com>
2021-04-05 02:55:34 +0530

    basic alert modified

--- lib/common/basic_alert.dart
+++ lib/common/basic_alert.dart
@@ -9,11 +9,14 @@ class BasicAlert extends StatelessWidget {
   String _no;
   Function _yesOnPressed;
   Function _noOnPressed;
+  bool isError;
 
   BasicAlert(this._title, this._content, this._yes, this._no,
-      this._yesOnPressed, this._noOnPressed);
+      this._yesOnPressed, this._noOnPressed, [this.isError = false]);
 
   TextStyle textStyle = TextStyle(color: Colors.black);
+  TextStyle contentStyle = TextStyle(color: Colors.black);
+  TextStyle errorStyle = TextStyle(color: Colors.redAccent);
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +25,7 @@ class BasicAlert extends StatelessWidget {
       filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
       child: AlertDialog(
         title: Text(this._title, style: textStyle),
-        content: Text(this._content, style: textStyle),
+        content: Text(this._content, style: this.isError ? errorStyle: contentStyle),
         shape:
             RoundedRectangleBorder(borderRadius: new BorderRadius.circular(15)),
         actions: [
